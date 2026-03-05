3cca83f4977b5fdad0730641da781e04fdd04463
arnaumontsia <arnaucaballer@iesmontisa.org>
2026-03-05 13:04:09 +0100

    Arxius amb informació sobre la utilitat dels directoris

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/') 
 def home():
-    return "Hola des de app.py" 
+    return render_template("login.html") 
 
 if __name__ == '__main__':
     app.run(debug=True)     
